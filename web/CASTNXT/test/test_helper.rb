4178f89c794621e5dc2487ad00cf71ddfdc9511a
Kelvin Wang <kelvinwop@gmail.com>
2023-04-05 09:35:37 -0700

    tests are now runnable using rails test, all tests failing but running now.

--- web/CASTNXT/test/test_helper.rb
+++ web/CASTNXT/test/test_helper.rb
@@ -1,10 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+
+# https://stackoverflow.com/questions/52959888/activerecordconnectionnotestablished-no-connection-pool-for-activerecordbas
+# ActiveRecord::ConnectionNotEstablished: No connection pool with 'primary' found.
+Object.send(:remove_const, :ActiveRecord)
+
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
 
+  # fixtures :all
   # Add more helper methods to be used by all tests here...
 end
